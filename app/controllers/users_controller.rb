00932da90f80f68b3eb47fc1275ad372af1b24f6
Justin Raines <justin.magtheridon@gmail.com>
2017-11-27 11:57:36 -0800

    adding in submission controllers, more style changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   private
 
   def admin_only
-    unless current_user.admin?
+    unless current_user.admin? 
       redirect_to root_path, :alert => "Access denied."
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
 
-
+  get 'submission' => 'submission#new'
   get 'test_cap' => 'application#test_cap'
 
 
