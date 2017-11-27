56592a06e18e347bb460f9777b36bf873ec93d87
Juan Alvarado <jalvarado@ctacorp.com>
2017-11-27 12:02:38 -0800

    Add API endpoint 'POST /api/v1/cap_alerts' for creating alerts

--- app/controllers/api/v1/cap_alerts_controller.rb
+++ app/controllers/api/v1/cap_alerts_controller.rb
@@ -1,13 +1,17 @@
 module Api::V1
   class CapAlertsController < ActionController::API
 
+    wrap_parameters format: [:json]
+
     def create
-      cap_alert = CapAlert.create cap_alert_params
+      cap_alert = CapBuilder.from_json(cap_alert_params)
       render json: cap_alert, status: :created
     end
 
+    private
+
     def cap_alert_params
-      params.require(:cap_alert).permit(:identifier)
+      params.require(:cap_alert).permit!
     end
 
   end
--- spec/controllers/api/v1/cap_alerts_controller_spec.rb
+++ spec/controllers/api/v1/cap_alerts_controller_spec.rb
@@ -1,21 +1,24 @@
 module Api::V1
   RSpec.describe CapAlertsController do
-    def valid_cap_alert
-      {
-        identifier: "test_alert"
-      }
-    end
     describe "POST #create" do
       context "with valid attributes" do
         it 'creates a new cap alert' do
-          expect {post :create, params: {cap_alert: valid_cap_alert}}.to change {CapAlert.count}
-        end
+          valid_cap_alert = {
+            cap_alert: {
+              identifier: "test_alert"
+            }
+          }
 
-        it 'returns a 201 CREATED' do
-          post :create, params: {cap_alert: valid_cap_alert}
-          expect(response).to have_http_status(201)
-        end
+          request_headers = {
+            "Accept" => "application/json",
+            "Content-Type" => "application/json"
+          }
 
+          request.headers.merge! request_headers
+
+          allow(CapBuilder).to receive(:from_json)
+          post :create, params: valid_cap_alert
+        end
       end
     end
   end
