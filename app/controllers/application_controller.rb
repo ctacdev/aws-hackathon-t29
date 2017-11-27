4bfd899559ac187c7a43bfafa8337450a08abacf
Justin Raines <justin.magtheridon@gmail.com>
2017-11-27 10:54:13 -0800

    add a test cap xml files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def test_cap
+    respond_to do |format|
+      format.xml { render :xml => CapBuilder.generate_message.to_xml }
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
+
+
+  get 'test_cap' => 'application#test_cap'
 end
+
